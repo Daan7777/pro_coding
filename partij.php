6d2cf251f942c2ec47fa9f59f1ba39cfdfaceea3
Daan7777 <Detvanmontfoort@gmail.com>
2022-02-09 13:30:58 +0100

    partij.php

---
+++ partij.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partij!</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div>
+        <a class="back" href="entercode.php">terug knop</a>
+    </div>
+    <div>
+        <p class="top"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+    </div>
+</body>
+</html>
