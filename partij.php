af2104fdd1ea929c3800a74a3841bee2b2149e07
Daan7777 <Detvanmontfoort@gmail.com>
2022-02-10 09:36:57 +0100

    partij pagina

--- partij.php
+++ partij.php
@@ -10,21 +10,78 @@
 </head>
 
 <body>
+    <!-- PHP dat je terugstuurd als je geen code hebt ingevuld. -->
     <?php
     if (empty($_SESSION)) session_start();
     // print_r($_SESSION);
     if (empty($_SESSION["code"])) header("Location: ./");
     ?>
+
     <div>
         <a class="back" href="entercode.php">terug knop</a>
     </div>
     <div>
         <p class="top"> Kies de partij waar u voor wilt stemmen. </p>
     </div>
-
-    <div>
-        <p> <img src="images/avatar.jpg" class="logo" alt="avatar pic" width="200" height="200"> tekst naast foto </p>
-        <a href="persoon.php" class="button"> button onder de foto</a>
+    <div> 
+        <table class="left"> <!-- partij links. -->
+            <tr>
+                <th> <img src="images/avatar.jpg" class="logo" alt="avatar pic" width="200" height="200"> </th>
+                <th> tekst naast foto. </th>
+            </tr>
+            <tr>
+                <th> <a href="persoon.php" class="button"> button onder de foto</a> </th>
+            </tr>
+        </table>
+        <table class="right"> <!-- partij rechts. -->
+            <tr>
+                <th> <img src="images/avatar.jpg" class="logo" alt="avatar pic" width="200" height="200"> </th>
+                <th> tekst naast foto. </th>
+            </tr>
+            <tr>
+                <th> <a href="persoon.php" class="button"> button onder de foto</a> </th>
+            </tr>
+        </table>
+    </div>
+    <div> 
+        <table class="left"> <!-- partij links. -->
+            <tr>
+                <th> <img src="images/avatar.jpg" class="logo" alt="avatar pic" width="200" height="200"> </th>
+                <th> tekst naast foto. </th>
+            </tr>
+            <tr>
+                <th> <a href="persoon.php" class="button"> button onder de foto</a> </th>
+            </tr>
+        </table>
+        <table class="right"> <!-- partij rechts. -->
+            <tr>
+                <th> <img src="images/avatar.jpg" class="logo" alt="avatar pic" width="200" height="200"> </th>
+                <th> tekst naast foto. </th>
+            </tr>
+            <tr>
+                <th> <a href="persoon.php" class="button"> button onder de foto</a> </th>
+            </tr>
+        </table>
+    </div>
+    <div> 
+        <table class="left"> <!-- partij links. -->
+            <tr>
+                <th> <img src="images/avatar.jpg" class="logo" alt="avatar pic" width="200" height="200"> </th>
+                <th> tekst naast foto. </th>
+            </tr>
+            <tr>
+                <th> <a href="persoon.php" class="button"> button onder de foto</a> </th>
+            </tr>
+        </table>
+        <table class="right"> <!-- partij rechts. -->
+            <tr>
+                <th> <img src="images/avatar.jpg" class="logo" alt="avatar pic" width="200" height="200"> </th>
+                <th> tekst naast foto. </th>
+            </tr>
+            <tr>
+                <th> <a href="persoon.php" class="button"> button onder de foto</a> </th>
+            </tr>
+        </table>
     </div>
 </body>
 
