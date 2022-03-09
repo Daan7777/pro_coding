dae10ba5ac1a979db4a4e5a88e7ece9b9b576e46
Daan7777 <Detvanmontfoort@gmail.com>
2022-03-09 13:57:16 +0100

    zelfde code als partij toegevoegd

--- persoon.php
+++ persoon.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>kandidaten!</title>
+    <title>partij!</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 
@@ -16,32 +16,46 @@
     // print_r($_SESSION);
     if (empty($_SESSION["code"])) header("Location: ./");
     ?>
-    <div>
-        <a class="back" href="entercode.php">terug knop</a>
+
+    <!-- Terug knop + tekst. -->
+    <div class="back left">
+        <img src="./images/go_back.png" alt="go_back" style="width:50px;height:50px;">
+        <a class="nodecoration big" href="entercode.php">ga terug</a>
     </div>
     <div>
         <p class="top"> Kies de persoon waar u voor wilt stemmen. </p>
     </div>
-    <div> <!-- persoon slogan en persoon naam moet uit de database komen. En als je die naam selecteerd moet naar de goeie mensen van die persoon pagina komen. -->
-        <table> <!-- persoon links. -->
-            <tr>
-                <th> <img src="images/avatar.jpg" class="logo" alt="avatar pic" width="200" height="200"> </th>
-                <th class="table_tekst"> {persoon slogan} Lorem ipsum dolor sit amet, consectetur adipiscing elit. </th>
-            </tr>
-            <tr>
-                <th> <a href="persoon.php" class="button">  {naam}</a> </th>
-            </tr>
-        </table>
-        <table> <!-- persoon rechts. -->
-            <tr>
-                <th> <img src="images/avatar.jpg" class="logo" alt="avatar pic" width="200" height="200"> </th>
-                <th class="table_tekst"> {persoon slogan} Lorem ipsum dolor sit amet, consectetur adipiscing elit. </th>
-            </tr>
-            <tr>
-                <th> <a href="persoon.php" class="button">  {naam}</a> </th>
-            </tr>
-        </table>
-    </div>
-</body>
 
-</html>
+    <!-- Database link -->
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "pro_coding";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM partijen" ;
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) { 
+            // var_dump( $row ) ;
+            echo "<div class='blok'>";
+            echo "    <img src='./partij_logos/".$row['id'].".png' alt='".$row['naam']." logo' class='partij'>";
+            echo "    <p class='rechts'>".$row['slogan']."</p>";
+            echo "    <a class='midden knop' href='persoon.php'>".$row['naam']."</a>";
+            echo "</div>";
+        }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+    ?>
+
+    
+</body>
