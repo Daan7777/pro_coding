630a812223902d368d19b097fc490c2e5d33a2c7
Daan7777 <Detvanmontfoort@gmail.com>
2022-03-09 13:29:37 +0100

    database connectie include aangepast

--- database_connectie.php
+++ database_connectie.php
@@ -16,22 +16,8 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
-        
-        ?>
-        <table class="left"> <!-- partij links. -->
-        <tr>
-            <th> <img src="images/avatar.jpg" class="logo" alt="avatar pic" width="200" height="200"> </th>
-            <th class="table_tekst"> {Partij slogan} Lorem ipsum dolor sit amet, consectetur adipiscing elit. </th>
-        </tr>
-        <tr>
-            <th> <a href="persoon.php" class="button"> <?php echo $row["naam"] ; ?></a> </th>
-        </tr>
-    </table>
-
-    <?php
-        
-       
+    while($row = $result->fetch_assoc()) { 
+        var_dump( $row )
     }
 } else {
     echo "0 results";
