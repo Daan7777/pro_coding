58260f806fc0e513ce0fdf5c667e4f659fe1e9d9
ArnoKeesmand <arnokeesman@gmail.com>
2022-03-10 11:43:24 +0100

    text on goodbye page

--- goodbye.php
+++ goodbye.php
@@ -64,10 +64,9 @@
 
     <p <?php if (!empty($errorMessage)) echo "style='color:red'" ?>>
         <?php 
-            if (empty($errorMessage)) echo "verhaal van max?";
+            if (empty($errorMessage)) echo "Bedankt voor het ingeven van uw stem.";
             else echo $errorMessage;
         ?>
-        <!-- Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Maecenas pharetra convallis posuere morbi. Dictum varius duis at consectetur lorem donec. Non pulvinar neque laoreet suspendisse interdum consectetur libero. Egestas pretium aenean pharetra magna ac placerat vestibulum lectus. Volutpat ac tincidunt vitae semper. Pharetra massa massa ultricies mi quis hendrerit dolor. Enim tortor at auctor urna nunc id cursus. Scelerisque fermentum dui faucibus in ornare quam viverra orci sagittis. Pretium vulputate sapien nec sagittis aliquam. Neque viverra justo nec ultrices. Amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar. -->
     </p>
     <div onclick="window.location.href = './';" class="progress">
         <span style="position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);">Terug</span>
